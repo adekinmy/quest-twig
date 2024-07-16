b9c6f180353b7389655f758186fce96d0f47d5ed
adekinmy <remy.bokini@gmx.fr>
2024-07-16 16:58:38 +0200

    Ajout du footer et de la page de détails avec adresse eamil de contact

--- config/twig.php
+++ config/twig.php
@@ -1,7 +1,17 @@
 <?php
 
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
-$twig = new Twig\Environment($loader, ['debug' => true]);
-$twig->addExtension(new Twig\Extension\DebugExtension());
+$loader = new FilesystemLoader(__DIR__ . '/../src/View');
+$twig = new Environment($loader, [
+    'debug' => true,
+]);
+
+$twig->addExtension(new DebugExtension());
+$twig->addGlobal('contactEmail', 'contact@monsite.com');
+
+return $twig;
